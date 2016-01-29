6753fcb72870549595aa9524df6d92b9572acfd7
random9s <jake.august.parham@gmail.com>
2016-01-28 19:20:30 -0600

    Updated for homework 1

---
+++ Homework1/hw1_sp16/src/MatrixMultiply.cpp
@@ -0,0 +1,42 @@
+#include "MatrixMultiply.hpp"
+
+#include <exception>
+#include <boost/numeric/ublas/matrix.hpp>
+#include <boost/numeric/ublas/matrix_proxy.hpp>
+#include <numeric>
+
+scottgs::MatrixMultiply::MatrixMultiply() 
+{
+	;
+}
+
+scottgs::MatrixMultiply::~MatrixMultiply()
+{
+	;
+}
+
+
+scottgs::FloatMatrix scottgs::MatrixMultiply::operator()(const scottgs::FloatMatrix& lhs, const scottgs::FloatMatrix& rhs) const
+{
+	// Verify acceptable dimensions
+	if (lhs.size2() != rhs.size1())
+		throw std::logic_error("matrix incompatible lhs.size2() != rhs.size1()");
+
+	scottgs::FloatMatrix result(lhs.size1(),rhs.size2());
+
+
+	// YOUR ALGORIHM WITH COMMENTS GOES HERE:
+
+
+	return result;
+}
+
+scottgs::FloatMatrix scottgs::MatrixMultiply::multiply(const scottgs::FloatMatrix& lhs, const scottgs::FloatMatrix& rhs) const
+{
+	// Verify acceptable dimensions
+	if (lhs.size2() != rhs.size1())
+		throw std::logic_error("matrix incompatible lhs.size2() != rhs.size1()");
+
+	return boost::numeric::ublas::prod(lhs,rhs);
+}
+
