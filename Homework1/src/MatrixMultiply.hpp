146013648a5b1f13e28f5250c781d1147c7ae188
random9s <jake.august.parham@gmail.com>
2016-02-03 23:48:50 -0600

    final optimization

--- Homework1/src/MatrixMultiply.hpp
+++ Homework1/src/MatrixMultiply.hpp
@@ -2,7 +2,7 @@
 #define _JPVC4_MATRIX_MULTIPLY
 
 #include "FloatMatrix.hpp"
-
+#define BLOCK_SIZE 64 
 namespace jpvc4 
 {
 
@@ -21,24 +21,34 @@ public:
 		// Verify acceptable dimensions
 		if (lhs.size2() != rhs.size1())
 			throw std::logic_error("matrix incompatible lhs.size2() != rhs.size1()");
+		
 		jpvc4::FloatMatrix result(lhs.size1(),rhs.size2());
 		jpvc4::FloatMatrix t_rhs(trans(rhs));	
 		const float* A = &(lhs(0,0)); 
 		const float* B = &(t_rhs(0,0));
 	  	float sum = 0;
 		const unsigned int lhs_row = lhs.size1(), lhs_column = lhs.size2(), rhs_column = rhs.size2(), t_rhs_column = t_rhs.size2(); 
+
 		// YOUR ALGORIHM WITH COMMENTS GOES HERE:
-		for(unsigned int i = 0; i<lhs_row; ++i){
-			for(unsigned int j = 0; j<rhs_column; ++j){
-			 	sum = 0;
-				const float* lhs_current_row = (A + (i * lhs_column));
-				const float* t_rhs_current_row = (B + (j * t_rhs_column));	
-				for(unsigned int k = 0; k<lhs_column; ++k){
+		for(unsigned int ii = 0; ii < lhs_row; ii+=BLOCK_SIZE){
+		    for(unsigned int jj = 0; jj < rhs_column; jj+=BLOCK_SIZE){
+		        for(unsigned int kk = 0; kk < lhs_column; kk+=BLOCK_SIZE){	
+        		    //Inner loops do ijk algorithm 
+			    for(unsigned int i = ii; i<ii+BLOCK_SIZE && i<lhs_row; ++i){
+			        for(unsigned int j = jj; j<jj+BLOCK_SIZE && j<rhs_column; ++j){
+			 	    sum = 0;
+				    const float* lhs_current_row = (A + (i * lhs_column));
+				    const float* t_rhs_current_row = (B + (j * t_rhs_column));	
+				    for(unsigned int k = kk; k<kk+BLOCK_SIZE && k<lhs_column; ++k){
 					sum += ( *(lhs_current_row + k) * *(t_rhs_current_row + k) );	
-				} 
-				result(i,j) = sum;
-			}
-		}	
+				    } 
+				    result(i,j) = sum;
+				}
+			    }
+		        }
+		    }
+		}
+		
 		return result;
 	}
 
